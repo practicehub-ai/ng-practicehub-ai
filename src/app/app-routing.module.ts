5f50c67d719a967911f32acf06906aef964ce583
practicehub-ai <codesoles@gmail.com>
2023-06-11 17:42:31 +1000

    Logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SubjectComponent } from './subject/subject.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, //default route to home
   { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
   { path: 'signup', component: SignUpComponent },
   { path: 'subject', component: SubjectComponent },
 
