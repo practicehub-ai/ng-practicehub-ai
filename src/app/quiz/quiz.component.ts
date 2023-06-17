10b53a9089ae81a40d745582bc092b84da0b96f9
practicehub-ai <codesoles@gmail.com>
2023-06-17 23:49:14 +0530

    Validation added on Submit, SID hardcoded for now

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -18,14 +18,17 @@ export class QuizComponent {
   questionCount = 0;
   selectedOption: any;
   selectionArray = [];
+  validation: any;
 
 
   constructor(private route: ActivatedRoute) {
     this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
-    this.route.queryParams.subscribe(params => {this.subid = params['subid'];});
+    this.route.queryParams.subscribe(params => { this.subid = params['subid']; });
   }
   async getQuestions() {
-    const { data, error } = await this.supabase.from('QNA').select('*').eq('sid', this.subid);
+    //const { data, error } = await this.supabase.from('QNA').select('*').eq('sid', this.subid);
+    //for now hard coded the sid, TBD
+    const { data, error } = await this.supabase.from('QNA').select('*').eq('sid', 1);
 
     if (error) {
       console.log(error);
@@ -57,6 +60,7 @@ export class QuizComponent {
 
 
   nextBtnHandler() {
+    this.commonFuncOnNxtPrv()
     if (this.selectedOption) {
       this.quiz[this.questionCount].userSelection = this.selectedOption;
     }
@@ -67,6 +71,7 @@ export class QuizComponent {
     console.log("nxt ", this.questionCount,)
   }
   previousBtnHandler() {
+    this.commonFuncOnNxtPrv()
     if (this.selectedOption) {
       this.quiz[this.questionCount].userSelection = this.selectedOption;
     }
@@ -77,8 +82,27 @@ export class QuizComponent {
   }
 
   submitBtnHandler() {
-    console.log(this.selectedOption)
+
+    let correctAnswer = this.checkUndefinedOrNullOrEmptyString(this.quiz[this.questionCount].answer) ? this.quiz[this.questionCount].answer : this.quiz[this.questionCount].answer.trim();
+    let userSelectedAnswer = this.checkUndefinedOrNullOrEmptyString(this.selectedOption) ? this.selectedOption : this.selectedOption.trim();
+    if (!this.checkUndefinedOrNullOrEmptyString(correctAnswer) && !this.checkUndefinedOrNullOrEmptyString(userSelectedAnswer)) {
+      this.validation = (correctAnswer === userSelectedAnswer) ? 'Correct Answer' : 'Oops! Not a right answer';
+    } else {
+      this.validation = 'Select an Answer';
+    }
   }
+
+  commonFuncOnNxtPrv() {
+    this.validation = '';
+  }
+
+  checkUndefinedOrNullOrEmptyString(value: any) {
+    if (value === undefined || value === null || value === '') {
+      return true;
+    }
+    return false;
+  }
+
 
 }
 
