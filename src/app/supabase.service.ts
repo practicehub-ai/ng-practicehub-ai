52f2882d57276d5f750349c42a78d14ca49cdbda
practicehub-ai <codesoles@gmail.com>
2023-07-02 18:29:11 +1000

    Update supabase.service.ts

--- src/app/supabase.service.ts
+++ src/app/supabase.service.ts
@@ -62,7 +62,7 @@ export class SupabaseService {
   }
   async signInWithProvider(provider: string): Promise<any> {    
     return this.supabaseClient.auth.signInWithOAuth({ provider: 'google', 
-                                                      options: { redirectTo:  "../subject" }
+                                                      options: { redirectTo:  "https://practicehubai.herokuapp.com/subject" }
     });    
   }
   public signOut(): Promise<any> {
