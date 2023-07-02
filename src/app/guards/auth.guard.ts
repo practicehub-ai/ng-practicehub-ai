263874d3b9fc35129fbba1b3163ff958e9d5610d
practicehub-ai <codesoles@gmail.com>
2023-07-02 19:48:55 +1000

    Update auth.guard.ts

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,26 +1,13 @@
 import { inject } from '@angular/core';
 import { CanActivateFn,Router } from '@angular/router';
-import { SupabaseService } from '../supabase.service';
-
 
 
 export const authGuard: CanActivateFn = (route, state) => {
   // Check if the user is logged in...
   // If not, redirect the user to the login page
   const router=inject(Router);
-
-  let user: any;
-  let loggedIn: any;
-  let supabaseService: SupabaseService = new SupabaseService();
-  supabaseService.authChanges((event, session) => {
-    user = session?.user;
-    loggedIn = (session?.user != null);
-    //console.log(session?.user)
-    localStorage.setItem('practiceUserId', JSON.stringify(session?.user?.id));
-  });
+  const loggedIn = localStorage.getItem('practiceUserId');
  
-  console.log("auth user: ",user);
-  console.log("auth loggedIn: ",loggedIn);
   console.log("Type of loggedIn ",typeof loggedIn);
   if (loggedIn !== undefined){
     console.log("true");
