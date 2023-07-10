87cfe77631a5985c874675613c198032930583e0
practicehub-ai <codesoles@gmail.com>
2023-07-11 00:33:52 +0530

    home page - subject displayed individually

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { SupabaseService } from '../supabase.service';
 import { Router } from '@angular/router';
+import { SupabaseClient, createClient } from '@supabase/supabase-js'
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -10,27 +12,51 @@ import { Router } from '@angular/router';
 export class HomeComponent {
   animateCount = 0; val = 0;
   interval: any;
-  maxQuestions = 200;
+  maxQuestions = 500;
+  private supabase: SupabaseClient;
+  subjects: any;
 
-  constructor(private router: Router,private supabaseService: SupabaseService) { }
-  
-  GoToPractice(){
+  constructor(private router: Router, private supabaseService: SupabaseService) {
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+  }
+
+  GoToPractice() {
     this.router.navigate(['/subject']);
   }
   user: any;
   loggedIn: any;
   ngOnInit() {
-    this.supabaseService.authChanges((event, session) => {     
+
+    this.supabaseService.authChanges((event, session) => {
       //console.log(event, session);
       this.user = session?.user;
       this.loggedIn = (session?.user != null);
 
-      if(this.loggedIn){
+      if (this.loggedIn) {
         this.router.navigate(['/subject']);
       }
     });
+    this.fetchSubjects();
     this.startTimer()
   }
+  async fetchSubjects() {
+    //subject data
+    const { data, error } = await this.supabase.from('Subjects').select('*');
+    this.subjects = data;
+    console.log(data);
+    for (let subject of this.subjects) {
+      this.fetchSubjectCount(subject);
+    }
+  }
+
+  async fetchSubjectCount(subject: any) {
+    //Query Need To Be Optimised
+    const { data, error } = (await this.supabase.from('QNA').select('*').eq('sid', subject.id));
+    subject.questionLength = data?.length;
+
+  }
+
+
   startTimer() {
     this.interval = setInterval(() => {
       this.animateCount += (2 * this.val);
